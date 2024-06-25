c3ad58e679d13b445da62b190222623522c941df
Alex Pavlov <yukiu217@gmail.com>
2024-06-26 01:38:38 +0600

    make base settings, add custom user

--- config/settings.py
+++ config/settings.py
@@ -11,9 +11,15 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+
+from config.utils import find_env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+ENV_DIR = Path(__file__).resolve() / '.env'
+
+load_dotenv(ENV_DIR)
 
 
 # Quick-start development settings - unsuitable for production
@@ -37,6 +43,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    # djangorestframework
+    'rest_framework',
+    
+    #custom apps
+    'users.apps.UsersConfig',
+    
 ]
 
 MIDDLEWARE = [
@@ -75,8 +88,13 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': find_env('DB_NAME'),
+        'USER': find_env('DB_USER'),
+        'HOST': find_env('DB_HOST'),
+        'PORT': find_env('DB_PORT'),
+        'PASSWORD': find_env('PASSWORD_POSTGRES'),
+        
     }
 }
 
@@ -103,7 +121,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 
@@ -121,3 +139,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.User'

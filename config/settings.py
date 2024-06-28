0659673c93de7b5212c9fb08922be4d29d113abe
Alex Pavlov <yukiu217@gmail.com>
2024-06-29 01:59:35 +0600

    add env.sample

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ load_dotenv(ENV_DIR)
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-05)sopmygf#7e0slcq9ipitwbgmtm44oms&=xt=8b5ds3vcl7g'
+SECRET_KEY = find_env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

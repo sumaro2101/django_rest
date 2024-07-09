17496f1ef7f65999e1ab7e24de931d0b807fa8b0
Alex Pavlov <yukiu217@gmail.com>
2024-07-09 12:36:53 +0600

    fix dublicate veiws into swagger

--- users/urls.py
+++ users/urls.py
@@ -21,5 +21,3 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
-
-urlpatterns = format_suffix_patterns(urlpatterns)
